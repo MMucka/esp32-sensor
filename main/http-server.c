fd4bdb2c552e90f0f0116e5b052d5299af77f8c4
Milan <milan@mucka.sk>
2021-10-15 20:09:56 +0200

    new http server

--- main/http-server.c
+++ main/http-server.c
@@ -1,128 +1,89 @@
 #include "http-server.h"
-
-#include <esp_http_server.h>
 #include <esp_log.h>
+
+#include "esp_system.h"
+
+#include "lwip/sys.h"
+#include "lwip/netdb.h"
+#include "lwip/api.h"
+#include "lwip/err.h"
+#include "string.h"
+
 
 #include "storage.h"
 
-#define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
-
-static httpd_handle_t server = NULL;
-static httpd_req_t *http_req;
-
-static const char *TAG = "HTTP_SERVER";
-
-
-/* An HTTP GET handler */
-static esp_err_t connect_get_handler(httpd_req_t *req)
+const static char http_html_hdr[] = "HTTP/1.1 200 OK\r\nContent-type: text/html\r\n\r\n";
+const uint8_t indexHtmlStart[] asm("_binary_index_html_start"); //uint8_t
+const uint8_t indexHtmlEnd[] asm("_binary_index_html_end");     //uint8_t
+   
+static void http_server_netconn_serve(struct netconn *conn)
 {
-    char*  buf;
-    size_t buf_len;
-    http_req = req;
+  struct netbuf *inbuf;
+  char *buf;
+  u16_t buflen;
+  err_t err;
+ 
+  /* Read the data from the port, blocking if nothing yet there.
+   We assume the request (the part we care about) is in one netbuf */
+  err = netconn_recv(conn, &inbuf);
 
-    /* Get header value string length and allocate memory for length + 1,
-     * extra byte for null termination */
-    buf_len = httpd_req_get_hdr_value_len(req, "Host") + 1;
-    if (buf_len > 1) {
-        buf = malloc(buf_len);
-        /* Copy null terminated value string into buffer */
-        if (httpd_req_get_hdr_value_str(req, "Host", buf, buf_len) == ESP_OK) {
-            ESP_LOGI(TAG, "Found header => Host: %s", buf);
+  if (err == ERR_OK) {
+    netbuf_data(inbuf, (void**)&buf, &buflen);
+
+    // strncpy(_mBuffer, buf, buflen);
+
+    /* Is this an HTTP GET command? (only check the first 5 chars, since
+    there are other formats for GET, and we're keeping it very simple )*/
+    printf("buffer = %s \n", buf);
+    if (buflen>=5 &&
+        buf[0]=='G' &&
+        buf[1]=='E' &&
+        buf[2]=='T' &&
+        buf[3]==' ' &&
+        buf[4]=='/' ) {
+          printf("buf[5] = %c\n", buf[5]);
+      /* Send the HTML header
+             * subtract 1 from the size, since we dont send the \0 in the string
+             * NETCONN_NOCOPY: our data is const static, so no need to copy it
+       */
+
+        netconn_write(conn, http_html_hdr, sizeof(http_html_hdr)-1, NETCONN_NOCOPY);
+        netconn_write(conn, indexHtmlStart, indexHtmlEnd-indexHtmlStart-1, NETCONN_NOCOPY);
+    }
+
+  }
+  /* Close the connection (server closes in HTTP) */
+  netconn_close(conn);
+  netbuf_delete(inbuf);
+}
+
+static void http_server(void *pvParameters)
+{
+    struct netconn *conn, *newconn;
+    err_t err;
+    conn = netconn_new(NETCONN_TCP);
+    
+    netconn_bind(conn, NULL, 80);
+    netconn_listen(conn);
+    do {
+        err = netconn_accept(conn, &newconn);
+        if (err == ERR_OK) {
+            http_server_netconn_serve(newconn);
+            netconn_delete(newconn);
         }
-        free(buf);
-    }
+    } while(err == ERR_OK);
 
-    char ssid[32];
-    char passwd[32];
-
-    buf_len = httpd_req_get_url_query_len(req) + 1;
-    if (buf_len > 1) {
-        buf = malloc(buf_len);
-        if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) {
-            ESP_LOGI(TAG, "Found URL query => %s", buf);
-            
-            /* Get value of expected key from query string */
-            if (httpd_query_key_value(buf, "ssid", ssid, sizeof(ssid)) == ESP_OK) {
-                ESP_LOGI(TAG, "Found URL query parameter => ssid=%s", ssid);
-            }
-            if (httpd_query_key_value(buf, "password", passwd, sizeof(passwd)) == ESP_OK) {
-                ESP_LOGI(TAG, "Found URL query parameter => password=%s", passwd);
-            }
-        }
-        free(buf);
-    }
-
-    storage_write("ssid", ssid);
-    storage_write("passwd", passwd);
-
-    const char* resp_str = (const char*) req->user_ctx;
-    httpd_resp_send(req, resp_str, HTTPD_RESP_USE_STRLEN);
-
-    if (httpd_req_get_hdr_value_len(req, "Host") == 0) {
-        ESP_LOGI(TAG, "Request headers lost");
-    }
-    esp_restart();
-
-    return ESP_OK;
+    netconn_close(conn);
+    netconn_delete(conn);
 }
-
-static const httpd_uri_t connect = {
-    .uri       = "/connect",
-    .method    = HTTP_GET,
-    .handler   = connect_get_handler,
-    .user_ctx  = "Connecting ...."
-};
-
-static esp_err_t default_get_handler(httpd_req_t *req){
-    const char* resp_str = (const char*) req->user_ctx;
-    httpd_resp_send(req, resp_str, HTTPD_RESP_USE_STRLEN);
-
-    return ESP_OK;
-}
-
-static const httpd_uri_t defaultPage = {
-    .uri       = "/",
-    .method    = HTTP_GET,
-    .handler   = default_get_handler,
-    .user_ctx  = "<!DOCTYPE html><html><head><title>ESP32 sensor</title><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" /><style></style></head><body><h1>Connect to Wifi</h1><form action=\"/connect\"><h2>SSID</h2><input type=\"text\" id=\"ssid\" name=\"ssid\"><br><br><h2>Password</h2><input type=\"text\" id=\"password\" name=\"password\"><br><br><input type=\"submit\" value=\"Connect\"></form></body></html>",
-};
-
-
-static esp_err_t temperature_get_handler(httpd_req_t *req){
-    const char* resp_str = (const char*) req->user_ctx;
-    httpd_resp_send(req, resp_str, HTTPD_RESP_USE_STRLEN);
-
-    return ESP_OK;
-}
-
-static const httpd_uri_t temperature = {
-    .uri       = "/temp",
-    .method    = HTTP_GET,
-    .handler   = temperature_get_handler,
-    .user_ctx  = "10°C",
-};
-
 
 void http_server_start()
 {
-    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
-    config.lru_purge_enable = true;
-
-    // Start the httpd server
-    ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
-    if (httpd_start(&server, &config) == ESP_OK) {
-        // Set URI handlers
-        ESP_LOGI(TAG, "Registering URI handlers");
-        httpd_register_uri_handler(server, &defaultPage);
-        httpd_register_uri_handler(server, &connect);
-        httpd_register_uri_handler(server, &temperature);
-    }
-
-    ESP_LOGI(TAG, "Error starting server!");
+    xTaskCreate(&http_server, "http_server", 2048, NULL, 5, NULL);
 }
 
 void http_server_stop()
 {
-    httpd_stop(server);
+    
 }
